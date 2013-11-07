b791cb14102b92ecdbeba0df53d3411408111e28
mouda <dses5211@gmail.com>
2013-11-07 21:18:36 +0800

    qqq

--- runSimulation/runCR/runCR_FR.95_Finsed/KM_DC/RunAll.sh
+++
@@ -1,5 +0,0 @@
-./winULSAkmeans4b2_DC 195 11 8 0 180 mapFile/mapFile_uni_195_r500/mapFile_uniR500_N195_2.txt 0.25 2 1 6 0.95 >>recordCRkmeans4b2_0.25.txt 
-./winULSAkmeans4b2_DC 195 11 8 0 180 mapFile/mapFile_uni_195_r500/mapFile_uniR500_N195_2.txt 0.3 2 1 6 0.95 >>recordCRkmeans4b2_0.3.txt 
-./winULSAkmeans4b2_DC 195 11 8 0 180 mapFile/mapFile_uni_195_r500/mapFile_uniR500_N195_2.txt 0.35 2 1 6 0.95 >>recordCRkmeans4b2_0.35.txt 
-./winULSAkmeans4b2_DC 195 11 8 0 180 mapFile/mapFile_uni_195_r500/mapFile_uniR500_N195_2.txt 0.4 2 1 6 0.95 >>recordCRkmeans4b2_0.4.txt 
-./winULSAkmeans4b2_DC 195 11 8 0 180 mapFile/mapFile_uni_195_r500/mapFile_uniR500_N195_2.txt 0.483 2 1 6 0.95 >>recordCRkmeans4b2_0.483.txt 
