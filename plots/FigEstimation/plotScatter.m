0941a44764d2fd7a9e57cec47b3714c0fe7ee222
mouda <dses5211@hotmail.com>
2013-11-12 18:23:17 +0800

    update

--- plots/FigEstimation/plotScatter.m
+++ plots/FigEstimation/plotScatter.m
@@ -1,1 +1,13 @@
 data = dlmread('data/estimation');
+
+dataLength = size(data,1);
+x = zeros(dataLength,1);
+y = zeros(dataLength,1);
+for i = 1:size(data,1)
+    x(i) = (data(i,2) - data(i,1))/data(i,1);
+    y(i) = (data(i,3) - data(i,1))/data(i,1);
+end
+
+scatter(x,y,'*');
+axis([-0.02 0.08 -0.05 1 ])
+grid on;
