e9b3208a5ce2cc91cde087dbfb05773e6ddc9a3f
mouda <dses5211@gmail.com>
2013-11-12 08:32:22 +0800

    update

---
+++ runSimulation/runTest/runHN_FR.95_CR.48_N50_Finsed/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+../../../ULSA4b5_DC/ULSA4b5_DC 50 19 8 0 180 ../../mapFile/mapFile_uni_50_r500/mapFile_uniR500_N50_1.txt 0.477 2 1 1 0.95 0 20000
+
+
---
+++ runSimulation/runTest/testTopo_KMDC/runlocal.sh
@@ -0,0 +1,3 @@
+#../../../ULSA4b7_NOGUIDE/ULSA4b7_NOGUIDE 195 19 8 0 180 ../../mapFile/mapFile_uni_195_r500/mapFile_uniR500_N195_1.txt 0.477 2 1 1 0.95 0 5000
+../../../ULSAkmeans_MC/ULSAkmeans_MC 195 8 8 0 180 ../../mapFile/mapFile_uni_195_r500/mapFile_uniR500_N195_1.txt 0.477 2 1 1 0.95 1 50 powerUpdateKmedoid 
+#../../../ULSA4b7_NOGUIDE/ULSA4b7_NOGUIDE 50 9 8 0 180 ../../mapFile/mapFile_uni_50_r500/mapFile_uniR500_N50_1.txt 0.477 2 1 1 0.95 0 5000
