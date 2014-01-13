b873dd0552557baabae131203c3aab85d84e54b9
mouda <dses5211@hotmail.com>
2014-01-13 18:01:12 +0800

    change program options

--- runSimulation/runTest/testScheduling/runlocal.sh
+++ runSimulation/runTest/testScheduling/runlocal.sh
@@ -1,5 +1,3 @@
-#../../../ULSA4b8_DC/ULSA4b8_DC 195 19 8 0 180 ../../mapFile/mapFile_uni_195_r500/mapFile_uniR500_N195_1.txt 0.477 2 1 1 0.95 0 0
-#../../../ULSA4b8_DC/ULSA4b8_DC 50 10 8 0 180 ../../mapFile/mapFile_uni_50_r500/mapFile_uniR500_N50_1.txt 0.477 2 1 1 0.95 0 0
-../../../schedule/simulator 50 10 8 0 180 ../../mapFile/mapFile_uni_50_r500/mapFile_uniR500_N50_1.txt 0.477 2 1 1 0.95 0 0
-../../../schedule/simulator 10 2 8 0 180 ../../mapFile/mapFile_uni_10_r150/mapFile_uniR150_N10_1.txt 0.477 2 1 1 0.95 0 0
-../../../schedule/simulator 10 2 8 0 180 ../../mapFile/mapFile_uni_10_r150/mapFile_uniR150_N10_1.txt 0.477 2 1 1 0.95 0 0
+../../../schedule/simulator -n 50 -H 10 -q 8 -p 0 -b 180.0 -t 0.01 -m ../../mapFile/mapFile_uni_50_r500/mapFile_uniR500_N50_1.txt -c 0.477 -f 0.95 -G Branchbound 
+../../../schedule/simulator -n 10 -H 2 -q 8 -p 0 -b 180.0 -t 0.01 -m ../../mapFile/mapFile_uni_10_r150/mapFile_uniR150_N10_1.txt -f 0.95 -c 0.477 -G Branchbound
+../../../schedule/simulator -n 10 -H 2 -q 8 -p 0 -b 180.0 -t 0.01 -m ../../mapFile/mapFile_uni_10_r150/mapFile_uniR150_N10_1.txt -f 0.95 -c 0.477 -G Baseline
--- schedule/main.cpp
+++ schedule/main.cpp
@@ -4,6 +4,8 @@
 #include <ctime>
 #include <cmath>
 #include <cstdlib>
+#include <boost/program_options.hpp>
+
 #include "map.h"
 #include "mapFactory.h"
 #include "clusterStructure.h"
@@ -16,86 +18,111 @@
 #define SA_INI_TEMP 3.0
 #define SA_FIN_TEMP 0.5
 
+namespace po = boost::program_options;
+
 using namespace std;
 int main(int argc, char *argv[])
 {
   //********************************************//
   //User-assigned parameters                    //
   //********************************************//
-
-  if(argc!=14)
-  {
-    cout <<"Usage: ULSA_DC_Estimate "         << endl;
-    cout <<"\t 1 [ totalNodes         ]" << endl;
-    cout <<"\t 2 [ maxChNum           ]" << endl;
-    cout <<"\t 3 [ quantizationBits   ]" << endl;
-    cout <<"\t 4 [ PowerMax(dbm)      ]" << endl;
-    cout <<"\t 5 [ Bandwidth(kHz)     ]" << endl;
-    cout <<"\t 6 [ FilePath           ]" << endl;
-    cout <<"\t 7 [ Correlation Factor ]" << endl;
-    cout <<"\t 8 [ MapIndex           ]" << endl;
-    cout <<"\t 9 [ outputIndex        ]" << endl;
-    cout <<"\t 10[ Simulation Time    ]" << endl;
-    cout <<"\t 11[ Fidelity Ratio     ]" << endl;
-    cout <<"\t 12[ Structure and Detail: 0 to turn off; 1 to turn on ]" << endl;
-    cout <<"\t 13[ Number of Iteration]"  << endl;
-
-
-    cout<<"Output 1: Struc, Detail, Matrics"<<endl;
-    cout<<"Output 2: Struc, Detail, Metrics, Metrics Every Round"<<endl;
-    cout<<"IniFlag: kmeans , HeadLimites"<<endl;
-    return 0;
-  }
-
-  const int maxChNum= atoi(argv[2]);
-  const int totalNodes=atoi(argv[1]);
-  const double quantizationBits=atof(argv[3]);
-  const double powerMaxDbm =atof(argv[4]);
-  const float powerMaxWatt = pow(10,((float)powerMaxDbm)/10) /1000;
+  int maxChNum;
+  int totalNodes;
+  double quantizationBits;
+  double powerMaxDbm;
   //we don't need to divide the BW(bandwidthKhz*1000);//Unit := Watt
-  const double bandwidthKhz =atof(argv[5]);
-  const double compressionRatio=atof(argv[7]);
-  const int outputControl=atoi(argv[9]);
-  const int simulationTime=atoi(argv[10]);
-  const double fidelityRatio=atof(argv[11]);
-  const int isDetailOn=atoi(argv[12]);
-  string mapFileName(argv[6]);
+  double bandwidthKhz;
+  double compressionRatio;
+  double txTimePerSlot;
+  int SAIter;
+  double fidelityRatio;
+  string mapFileName;
+  string strAlgFlag;
+  try {
+    po::options_description desc("Allowed options");
+    desc.add_options()
+      ("help,h", "Produce help message")
+      ("bandwidth,b",     po::value<double>(),  "Bandwidth (kHz) ")
+      ("power,p",         po::value<double>(),  "Maximum Power (dbm) ")
+      ("quantization,q",  po::value<double>(),  "Bits of quantization")
+      ("map,m",           po::value<string>(),  "Map file name")
+      ("algorithm,G",     po::value<string>(),  "Scheduling algorithm Baseline|Algorithm ")
+      ("nodes,n",         po::value<int>(),     "Initial number of nodes")
+      ("heads,H",         po::value<int>(),     "Initial number of heads")
+      ("time,t",          po::value<double>(),  "Transmission time per slot (ms) ")
+      ("fidelity,f",      po::value<double>(),  "Fidelity ratio")
+      ("iteration,i",     po::value<int>(),     "Number of iteration Simulate Annealing")
+      ("correlation,c",   po::value<double>(),  "Correlation level");
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
 
+    if (vm.size() == 0 || vm.count("help")) {
+      cout << desc << "\n";
+      return 0;
+    } else if(vm.size() == 10 ) {
 
-  MapFactory myMapFactory(mapFileName, powerMaxWatt, 
-      compressionRatio, quantizationBits, bandwidthKhz, maxChNum, totalNodes);
-  Map* myMap = 0;
-  CORRE_MA_OPE* myMatComputer  = 0;
-  myMap = myMapFactory.CreateMap();
-  myMatComputer = myMapFactory.CreateMatrixComputer();
-  if (!myMap ) {
-    cerr << "Error: Failed to initialize map" << endl;
+      totalNodes =        vm["nodes"].as<int>();
+      maxChNum =          vm["heads"].as<int>();
+      quantizationBits =  vm["quantization"].as<double>();
+      powerMaxDbm =       vm["power"].as<double>();
+      bandwidthKhz =      vm["bandwidth"].as<double>();
+      txTimePerSlot =     vm["time"].as<double>();
+      compressionRatio =  vm["correlation"].as<double>();
+      fidelityRatio =     vm["fidelity"].as<double>();
+      mapFileName =       vm["map"].as<string>();
+      strAlgFlag =        vm["algorithm"].as<string>();
+
+      double powerMaxWatt = pow(10,(powerMaxDbm)/10) /1000;
+
+      MapFactory myMapFactory(mapFileName, powerMaxWatt, 
+          compressionRatio, quantizationBits, bandwidthKhz, maxChNum, totalNodes);
+      Map* myMap = 0;
+      CORRE_MA_OPE* myMatComputer  = 0;
+      myMap = myMapFactory.CreateMap();
+      myMatComputer = myMapFactory.CreateMatrixComputer();
+      if (!myMap ) {
+        cerr << "Error: Failed to initialize map" << endl;
+        return 1;
+      }
+      if (!myMatComputer) {
+        cerr << "Error: Failed to initialize correlation comulter" << endl;
+        return 1;
+      }
+      ClusterStructure* myCS = 0;
+      CsFactory myCsFactory(myMap, myMatComputer);
+      myCS = myCsFactory.CreateClusterStructure();
+
+      if (!myCS) {
+        cerr << "Error: Failed to initalize cluster structure" << endl;
+        return 1;
+      }
+
+      SchedulerFactory mySchedFactory(0.01, bandwidthKhz, myMap, myMatComputer, myCS);
+      Scheduler* myScheduler = 0;
+      myScheduler = mySchedFactory.CreateScheduler(strAlgFlag);
+      if (!myScheduler) {
+        cerr << "Error: Failed to initialize scheduler" << endl;
+        return 1;
+      }
+      Simulator mySimulator(myMap, myCS, myScheduler, myMatComputer);
+      mySimulator.SelfCheck();
+      mySimulator.Run(1);
+    }
+    else {
+      cout << desc << "\n";
+      return 0;
+    }
+
+  }
+  catch(std::exception& e) {
+    cerr << "error: " << e.what() << "\n";
     return 1;
   }
-  if (!myMatComputer) {
-    cerr << "Error: Failed to initialize correlation comulter" << endl;
-    return 1;
-  }
-  ClusterStructure* myCS = 0;
-  CsFactory myCsFactory(myMap, myMatComputer);
-  myCS = myCsFactory.CreateClusterStructure();
-  
-  if (!myCS) {
-    cerr << "Error: Failed to initalize cluster structure" << endl;
-    return 1;
+  catch(...) {
+    cerr << "Exception of unknown type!\n";
   }
 
-  SchedulerFactory mySchedFactory(0.05, bandwidthKhz, myMap, myMatComputer, myCS);
-  Scheduler* myScheduler = 0;
-  //myScheduler = mySchedFactory.CreateScheduler("Baseline");
-  myScheduler = mySchedFactory.CreateScheduler("Branchbound");
-  if (!myScheduler) {
-    cerr << "Error: Failed to initialize scheduler" << endl;
-    return 1;
-  }
-  Simulator mySimulator(myMap, myCS, myScheduler, myMatComputer);
-  mySimulator.SelfCheck();
-  mySimulator.Run(1);
   
   return 0;
 }
