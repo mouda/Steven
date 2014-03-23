909bd515857fc67c0d9bac527d7f73c0d0ce02c8
mouda <dses5211@hotmail.com>
2014-03-23 21:55:43 +0800

    update

---
+++ plots/FigTimeVar/sequential.m
@@ -0,0 +1,17 @@
+clear;
+close all;
+x = 0.0:0.1:0.5;
+baselineEntropy = dlmread('data/entropy_Baseline_Q_16_N50.out');
+branchboundEntropy = dlmread('data/entropy_Branchbound_Q_16_N50.out');
+greedyPhysical = dlmread('data/entropy_GreedyPhysical_Q_16_N50.out');
+
+str=sprintf('Branch and bound algorithm');
+plot(x,mean(branchboundEntropy,1),'^--','LineWidth',2.0,'Color','b','DisplayName',str,'MarkerSize',10); hold on;
+str=sprintf('GreedyPhysical algorithm');
+plot(x,mean(greedyPhysical,1),'^--','LineWidth',2.0,'Color','g','DisplayName',str,'MarkerSize',10);hold on;
+str=sprintf('Baseline algorithm');
+plot(x,mean(baselineEntropy,1),'*-','LineWidth',2.0,'Color','r','DisplayName',str,'MarkerSize',10); 
+legend('show');
+xlabel('Time (ms)');
+ylabel('Entropy (bits)');
+grid on;
