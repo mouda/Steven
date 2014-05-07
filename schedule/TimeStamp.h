45ccaaa1257f26ba3842617be17d7e70ff168848
mouda <dses5211@hotmail.com>
2014-05-07 13:58:53 +0800

    update

---
+++ schedule/TimeStamp.h
@@ -0,0 +1,9 @@
+#ifndef TIMESTAMP_H_INCLUDED
+#define TIMESTAMP_H_INCLUDED
+#include <ctime>
+
+class TimeStamp{
+    public:
+        void returnRealWordTime(char timeBuf[],int sizeofBuffer);
+};
+#endif // TIMESTAMP_H_INCLUDED
