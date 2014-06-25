c2671fda8fc63ea5393b581ba77a13039b81e6b7
mouda <dses5211@gmail.com>
2014-06-25 23:43:48 +0800

    update

---
+++ runSimulation/runTest/testPowerScheduling/runBest.sh
@@ -0,0 +1,1 @@
+  ../../../schedule/simulator -n 50 -H 13 -q 32 -p 0 -b 180.0 -t 0.1 -I 10.0 -m  ../../mapFile/mapFile_uni_50_r150/mapFile_uniR150_N50_6.txt -f 0.9 -c 0.477 -A MinPower  -T 1.0 -F CSFile -N 1 -P test.out -S testSolution.out -s cs/test_CS_N50_H13_new_6_F0.8.out
