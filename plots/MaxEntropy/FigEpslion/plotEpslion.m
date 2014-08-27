dc44efef3bb3e4dbf461bb88d67566d58365a004
mouda <dses5211@gmail.com>
2014-08-27 21:10:37 +0800

    update

--- plots/MaxEntropy/FigEpslion/plotEpslion.m
+++ plots/MaxEntropy/FigEpslion/plotEpslion.m
@@ -1,7 +1,7 @@
 clear; close all;
 cellStrEpslion = {'0.001','0.01','0.1','1','10','20'};
 numEpslion = [0.001 0.01, 0.1, 1, 10, 20];
-bbEntropy = zeros(1,length(cellStrEpslion));
+bbRation = zeros(1,length(cellStrEpslion));
 
 % Read Branch and Bound Algorithm
 for i = 1:length(cellStrEpslion)
@@ -19,12 +19,13 @@ for i = 1:length(cellStrEpslion)
             entropy = entropy + data;
         end
     end
-    bbEntropy(i) = entropy/counter;
+    bbRation(i) = entropy/counter;
 end
 
-gapRatio = 1 - bbEntropy;
+gapRatio = 1 - bbRation;
 
 plot(numEpslion,gapRatio,'-','LineWidth',2,'Color','b');
+title('Correlation level $\lambda = 0.477$, $M=50$','interpreter','latex');
 ylabel('Performance gap (%)');
 xlim([-0.2 20]);
 xlabel('Approximation parameter $(\epsilon)$','interpreter','latex');
